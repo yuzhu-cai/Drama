6d581645595c1ffd28a9f8b0030011ad91b702c0
yuzhu-cai <caiyuzhu.cs@gmail.com>
2025-01-11 15:07:15 +0800

    init framework

---
+++ config/varibles.py
@@ -0,0 +1,5 @@
+import os
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+OPENAI_BASE_URL = os.getenv('OPENAI_BASE_URL')
+
---
+++ utils/llm_call.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+from config.varibles import OPENAI_API_KEY, OPENAI_BASE_URL
+
+class OpenAILLM:
+    def __init__(
+            self,
+            system_prompt: str = "You are a helpful assistant!"
+    ):
+        self.client = OpenAI(
+            api_key=OPENAI_API_KEY,
+            base_url=OPENAI_BASE_URL
+        )
+        self.system_prompt = system_prompt
+
+
+    def call(
+            self, 
+            query: str, 
+            model: str="gpt-4o-mini", 
+            temperature: float = 0.2, 
+            top_p: float = 1.0, 
+            max_tokens: int = 4096, 
+            frequency_penalty: float = 0.0
+    ):
+        message = [
+            {"role": "system", "content": self.system_prompt},
+            {"role": "user", "content": query}
+        ]
+        completion = self.client.chat.completions.create(
+            model=model,
+            messages=message,
+            temperature=temperature,
+            top_p=top_p,
+            max_tokens=max_tokens,
+            frequency_penalty=frequency_penalty
+        )
+        
+        return completion.choices[0].message.content
+
+
+if __name__ == "__main__":
+    llm = OpenAILLM()
+    query = "1+1=?"
+    print(llm.call(query))
